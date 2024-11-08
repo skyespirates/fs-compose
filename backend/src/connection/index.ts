e76f2aec994f39993f8f4c6460b7f2fa9de6da0c
Aziz <akashiseijurou414@gmail.com>
2024-11-08 07:17:34 +0700

    add redis service

---
+++ backend/src/connection/index.ts
@@ -0,0 +1,71 @@
+import { createClient } from 'redis';
+import { connect } from 'mongoose';
+
+class MongoClient {
+  url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/mydb';
+  async connect() {
+    try {
+      await connect(this.url, {
+        serverSelectionTimeoutMS: 4000,
+        heartbeatFrequencyMS: 2000,
+      });
+      console.log('Connected to MongoDB ✅');
+    } catch (error: any) {
+      console.error('Error connecting to MongoDB:', error?.message);
+    }
+  }
+}
+
+class RedisClient {
+  url = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+  connected!: number;
+  conn!: any;
+  async connect() {
+    try {
+      const conn = await createClient({
+        url: this.url,
+        socket: {
+          connectTimeout: 5000,
+          reconnectStrategy: function (retries) {
+            if (retries > 3) {
+              console.log(
+                'Too many attempts to reconnect. Redis connection was terminated'
+              );
+              return new Error('Too many retries.');
+            } else {
+              return retries * 10;
+            }
+          },
+        },
+      });
+      conn.on('error', (err) => {
+        this.connected = 0;
+        throw err;
+      });
+      conn.connect();
+      conn.on('connect', () => {
+        console.log('Connected to Redis 👍 ');
+      });
+      conn.on('ready', () => {
+        this.connected = 1;
+        this.conn = conn;
+        console.log('Redis is ready to execute commands 🐱‍👤');
+      });
+    } catch (error: any) {
+      console.log('Error connecting to Redis -> ', error?.message);
+    }
+  }
+  async set(key: string, value: string) {
+    try {
+      const temp = await this.conn.set(key, value);
+      console.log('result ', temp);
+    } catch (error) {
+      console.log('error during set command ', error);
+    }
+  }
+}
+
+const mongoClient = new MongoClient();
+const redisClient = new RedisClient();
+
+export { mongoClient, redisClient };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,34 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import Task from "./models/Task.model";
+import express from 'express';
+import cors from 'cors';
+import Task from './models/Task.model';
+import { mongoClient, redisClient } from './connection/';
 
-const MONGO_URL = process.env.MONGO_URL as string;
-
-mongoose
-  .connect(MONGO_URL)
-  .then(() => {
-    console.log("Connected to MongoDB ✅");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+mongoClient.connect();
+redisClient.connect();
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     const exisingTasks = await Task.find({});
     const task = await Task.create({
-      title: "Task 1",
-      description: "Description 1",
+      title: 'Task 1',
+      description: 'Description 1',
       completed: false,
     });
     const data = {
-      message: "Hello World!",
+      message: 'Hello World!',
       exisingTasks,
     };
+    redisClient.set('name', 'brody');
     res.json(data);
   } catch (error) {
-    console.log("Error", error);
+    console.log('Error', error);
   }
 });
 
 app.listen(3001, () => {
-  console.log("Server 🚀 started on port 3001");
+  console.log('Server 🚀 started on port 3001');
 });
