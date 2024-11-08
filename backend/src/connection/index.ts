0a565d28f1bf0ead1262de4e3061af17b47ef868
Aziz <akashiseijurou414@gmail.com>
2024-11-08 09:57:38 +0700

    update route and add volume on services

--- backend/src/connection/index.ts
+++ backend/src/connection/index.ts
@@ -1,23 +1,23 @@
-import { createClient } from 'redis';
-import { connect } from 'mongoose';
+import { createClient } from "redis";
+import { connect } from "mongoose";
 
 class MongoClient {
-  url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/mydb';
+  url = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/mydb";
   async connect() {
     try {
       await connect(this.url, {
         serverSelectionTimeoutMS: 4000,
         heartbeatFrequencyMS: 2000,
       });
-      console.log('Connected to MongoDB ✅');
+      console.log("Connected to MongoDB ✅");
     } catch (error: any) {
-      console.error('Error connecting to MongoDB:', error?.message);
+      console.error("Error connecting to MongoDB:", error?.message);
     }
   }
 }
 
 class RedisClient {
-  url = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+  url = process.env.REDIS_URL || "redis://127.0.0.1:6379";
   connected!: number;
   conn!: any;
   async connect() {
@@ -29,38 +29,47 @@ class RedisClient {
           reconnectStrategy: function (retries) {
             if (retries > 3) {
               console.log(
-                'Too many attempts to reconnect. Redis connection was terminated'
+                "Too many attempts to reconnect. Redis connection was terminated"
               );
-              return new Error('Too many retries.');
+              return new Error("Too many retries.");
             } else {
               return retries * 10;
             }
           },
         },
       });
-      conn.on('error', (err) => {
+      conn.on("error", (err) => {
         this.connected = 0;
         throw err;
       });
       conn.connect();
-      conn.on('connect', () => {
-        console.log('Connected to Redis 👍 ');
+      conn.on("connect", () => {
+        console.log("Connected to Redis 👍 ");
       });
-      conn.on('ready', () => {
+      conn.on("ready", () => {
         this.connected = 1;
         this.conn = conn;
-        console.log('Redis is ready to execute commands 🐱‍👤');
+        console.log("Redis is ready to execute commands 🐱‍👤");
       });
     } catch (error: any) {
-      console.log('Error connecting to Redis -> ', error?.message);
+      console.log("Error connecting to Redis -> ", error?.message);
     }
   }
   async set(key: string, value: string) {
     try {
-      const temp = await this.conn.set(key, value);
-      console.log('result ', temp);
+      const temp = await this.conn.setEx(key, 120, value);
     } catch (error) {
-      console.log('error during set command ', error);
+      console.log("error during set command ", error);
+    }
+  }
+
+  async get(key: string) {
+    try {
+      const temp = await this.conn.get(key);
+      const result = JSON.parse(temp);
+      return result;
+    } catch (error) {
+      console.log("error during get command ", error);
     }
   }
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import Task from './models/Task.model';
-import { mongoClient, redisClient } from './connection/';
+import express from "express";
+import cors from "cors";
+import Task from "./models/Task.model";
+import { mongoClient, redisClient } from "./connection/";
 
 mongoClient.connect();
 redisClient.connect();
@@ -10,25 +10,48 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
+    // always retrieve from database
     const exisingTasks = await Task.find({});
-    const task = await Task.create({
-      title: 'Task 1',
-      description: 'Description 1',
-      completed: false,
-    });
     const data = {
-      message: 'Hello World!',
+      message: "Hello World!",
       exisingTasks,
     };
-    redisClient.set('name', 'brody');
     res.json(data);
   } catch (error) {
-    console.log('Error', error);
+    console.log("Error during getting tasks", error);
+  }
+});
+
+app.get("/add-task", async (req, res) => {
+  const random = Math.round(Math.random() * 100);
+  const task = {
+    title: `Task ${random}`,
+    description: `Description ${random}`,
+    completed: random % 2 === 0 ? true : false,
+  };
+  try {
+    await Task.create(task);
+    const existingTasks = await Task.find({});
+    // update redis cache every time a new task is created
+    redisClient.set("data", JSON.stringify(existingTasks));
+    res.json(existingTasks);
+  } catch (error) {
+    console.log("Error during creating task", error);
+  }
+});
+
+app.get("/cache", async (req, res) => {
+  try {
+    // get data from cache
+    const cacheData = await redisClient.get("data");
+    res.json(cacheData);
+  } catch (error) {
+    console.log("Error during getting cache", error);
   }
 });
 
 app.listen(3001, () => {
-  console.log('Server 🚀 started on port 3001');
+  console.log("Server 🚀 started on port 3001");
 });
